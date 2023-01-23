348c28ff143b084a007ea5b7d734d819b1e78375
hossainalamin <hossainalamin980@gmail.com>
2023-01-23 23:59:07 +0600

    Bootstarp with react

--- src/components/Faculties/Faculties.js
+++ src/components/Faculties/Faculties.js
@@ -2,34 +2,24 @@ import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
-
+import './Faculties.css';
 const Faculties = () => {
     const teachers = useLoaderData();
     return (
         <div className='flex'>
             <h2>Most Senior Faculty Members</h2>
-            <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-            </Card>
-            <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-            </Card>
+            {
+                teachers.map(teacher => <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={teacher.img} />
+                <Card.Body>
+                    <Card.Title>Name : {teacher.name}</Card.Title>
+                    <Card.Text>
+                    {teacher.designation}
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+                </Card>
+            )}
         </div>
     );
 };
