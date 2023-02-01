a87bbea6592c1ce70f8d1091e01a35c43def2b1d
hossainalamin <hossainalamin980@gmail.com>
2023-02-02 00:01:47 +0600

    firebase authentication

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,11 @@
 import React, { createContext} from 'react';
 import About from '../About/About';
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import app from '../../firebase/firebase.init';
+const auth = getAuth(app);
 export const UserContext = createContext();
 const Home = () => {
+const provider  = new GoogleAuthProvider();
     return (
         <UserContext.Provider value="Rokib">
             <About></About>
---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBqSM-bteWwe5yz4cGqa0CRuLUjc1czxd8",
+  authDomain: "cse-jnu-7f5c8.firebaseapp.com",
+  projectId: "cse-jnu-7f5c8",
+  storageBucket: "cse-jnu-7f5c8.appspot.com",
+  messagingSenderId: "25062144355",
+  appId: "1:25062144355:web:b45b182cd23ddc118c3b56"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
