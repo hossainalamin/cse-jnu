5c8ae18f9e31aa86f7cfe684563c93c7ce06de76
hossainalamin <hossainalamin980@gmail.com>
2023-02-04 14:16:35 +0600

    sign in with google

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,27 @@
 import React, { createContext} from 'react';
 import About from '../About/About';
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import app from '../../firebase/firebase.init';
 const auth = getAuth(app);
 export const UserContext = createContext();
 const Home = () => {
-const provider  = new GoogleAuthProvider();
+    const provider  = new GoogleAuthProvider();
+    const handleGoogleSignIn = ()=>{
+        signInWithPopup(auth, provider)
+        .then(result =>{
+            console.log(result.user);
+        })
+        .catch(error =>{
+            console.log('Error : '+error);
+        })
+    }
     return (
+        <div>
         <UserContext.Provider value="Rokib">
             <About></About>
         </UserContext.Provider>
+        <button onClick={handleGoogleSignIn}>Google Sign In</button>
+        </div>
     );
 };
 
