8e40f8d4f35d269b93904b4757863a822529eb46
hossainalamin <hossainalamin980@gmail.com>
2023-02-04 15:04:06 +0600

    sign in & sign out

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,18 +1,28 @@
-import React, { createContext} from 'react';
+import React, { createContext, useState} from 'react';
 import About from '../About/About';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import app from '../../firebase/firebase.init';
 const auth = getAuth(app);
 export const UserContext = createContext();
 const Home = () => {
+    const [user, setUser] = useState({});
     const provider  = new GoogleAuthProvider();
     const handleGoogleSignIn = ()=>{
         signInWithPopup(auth, provider)
         .then(result =>{
-            console.log(result.user);
+            setUser(result.user);
         })
         .catch(error =>{
             console.log('Error : '+error);
+        })
+    }
+    const handleGoogleSignOut = ()=> 
+    {
+        signOut(auth).then(()=>{
+            setUser({});
+        })
+        .catch(()=>{
+            setUser({});
         })
     }
     return (
@@ -20,7 +30,18 @@ const Home = () => {
         <UserContext.Provider value="Rokib">
             <About></About>
         </UserContext.Provider>
-        <button onClick={handleGoogleSignIn}>Google Sign In</button>
+        <hr></hr>
+        {user.email && <div>
+            <h3>User Name: {user.displayName}</h3>
+            <p>User Email: {user.email}</p>
+        </div>
+        }
+        {user.email ? 
+            <button onClick={handleGoogleSignOut}>Sign Out</button>
+            :
+            <button onClick={handleGoogleSignIn}>Sign In</button>
+        }
+        
         </div>
     );
 };
