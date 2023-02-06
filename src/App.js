a63fe89bc400df0907bf69b05db4399415e9b929
hossainalamin <hossainalamin980@gmail.com>
2023-02-06 23:47:25 +0600

    login form using bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { RouterProvider } from 'react-router-dom';
 import './App.css';
 import router from './Routes/Route'
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
-    <div className="App">
+    <div >
       <RouterProvider router={router}></RouterProvider>
     </div>
   );
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,7 @@
 import React, { createContext, useState} from 'react';
-import About from '../About/About';
 import { getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import app from '../../firebase/firebase.init';
+import LoginFrom from '../LoginFrom/LoginFrom';
 const auth = getAuth(app);
 export const UserContext = createContext();
 const Home = () => {
@@ -37,10 +37,6 @@ const Home = () => {
     }
     return (
         <div>
-        <UserContext.Provider value="Rokib">
-            <About></About>
-        </UserContext.Provider>
-        <hr></hr>
         {user.uid && <div>
             <h3>User Name: {user.displayName}</h3>
             <p>User Email: {user.email}</p>
@@ -50,10 +46,12 @@ const Home = () => {
         {user.uid ? 
             <button onClick={handleGoogleSignOut}>Sign Out</button>
             :
-            <>
+            <div className = "mx-auto w-50">
+            <LoginFrom></LoginFrom>
+            <br></br>
             <button onClick={handleGoogleSignIn}>Google Sign In</button>
             <button onClick={handleGitSignIn}>Github Sign In</button>
-            </>
+            </div>
         }
         
         </div>
