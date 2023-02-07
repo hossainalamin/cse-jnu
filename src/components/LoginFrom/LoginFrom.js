d803e2c0ad70cdaad72cab3ce930d42653b736b2
hossainalamin <hossainalamin980@gmail.com>
2023-02-07 23:06:44 +0600

    validation

--- src/components/LoginFrom/LoginFrom.js
+++ src/components/LoginFrom/LoginFrom.js
@@ -1,20 +1,30 @@
 import { createUserWithEmailAndPassword, getAuth} from 'firebase/auth';
-import React from 'react';
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import app from '../../firebase/firebase.init';
 const auth = getAuth(app);
 const LoginFrom = () => {
+    const [validation, setValidation] = useState('');
+    const [success, setSuccess] = useState(false);
     const getRegisterData = (event) =>{
+        setSuccess(false);
         event.preventDefault();
-        const email = event.target.email.value;
-        const password = event.target.password.value;
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        if(!/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W])/.test(password)){
+            setValidation('Please enter al least one upper letter one lower letter and password length between 6 to 20');
+            return;
+        }
         createUserWithEmailAndPassword(auth, email, password)
         .then((user) => {
-            console.log(user);
+            setValidation('');
+            setSuccess(true);
+            form.reset();
         })
-        .catch(()=>{
-
+        .catch((error)=>{
+            setValidation(error.message)
         })
     }
     return (
@@ -29,6 +39,12 @@ const LoginFrom = () => {
                     <Form.Label>Password</Form.Label>
                     <Form.Control type="password" name="password" placeholder="Password" />
                 </Form.Group>
+                <p className='text-danger'>
+                {validation}
+                </p>
+                {success && <p className='text-success'>User Created successfully</p>
+                }
+
                 <Button variant="primary" type="submit">
                     Register
                 </Button>
