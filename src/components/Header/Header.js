a5b9a92d89219a323635d3219a157c88fe8a540a
hossainalamin <hossainalamin980@gmail.com>
2023-02-09 23:31:12 +0600

    tailwind and daisy ui

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,6 +7,15 @@ const Header = () => {
             <div className='bg-img'>
                 <img src="https://picsum.photos/1400/300"alt=''></img>
             </div>
+            <div className="navbar bg-neutral text-neutral-content">
+            <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+            </div>
+            <div className="navbar bg-base-300">
+            <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+            </div>
+            <div className="navbar bg-primary text-primary-content">
+            <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+            </div>
             <nav className='nav-bar'>
                 <NavLink to="/" className = {({isActive}) => isActive ? 'active' : ''}>Home</NavLink>
                 <NavLink to="/about" className = {({isActive}) => isActive ? 'active' : ''}>About</NavLink>
