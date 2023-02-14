e032075eca00eec194dbf01387d0440fc0ed2530
hossainalamin <hossainalamin980@gmail.com>
2023-02-14 23:57:07 +0600

    login form

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,22 +3,15 @@ import './Header.css';
 const Header = () => {
     return (
         <div>
-            <div className="navbar bg-neutral text-neutral-content">
+            <div className="navbar bg-primary text-primary-content">
                 <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
-                <Link to="/">Home</Link>
-                <Link to="/login">Log In</Link>
-                <Link to="/register">Register</Link>
-            </div>
-            {/* <div className="navbar bg-primary text-primary-content">
-                <Link className="btn btn-ghost normal-case text-xl">CSE JNU</Link>
-                <Link to="/" className = {({isActive}) => isActive ? 'active' : ''}>Home</Link>
-                <Link to="/login" className = {({isActive}) => isActive ? 'active' : ''}>Login</Link>
-                <Link to="/register" className = {({isActive}) => isActive ? 'active' : ''}>Register</Link>
-                <Link to="/about" className = {({isActive}) => isActive ? 'active' : ''}>About</Link>
-                <Link to="/course" className = {({isActive}) => isActive ? 'active' : ''}>Courses</Link>
-                <Link to="/faculties"className = {({isActive}) => isActive ? 'active' : ''}>Faculties</Link>
-                <Link to="/hero"className = {({isActive}) => isActive ? 'active' : ''}>Our Heros</Link>
-            </div> */}
+                <div>
+            <Link to="/" className='navStyle'>Home</Link>
+                <Link to="/login" className='navStyle'>Log In</Link>
+                <Link to="/register" className='navStyle'>Register</Link>
+                </div>
+                </div>
+            
         </div>
     );
 };
